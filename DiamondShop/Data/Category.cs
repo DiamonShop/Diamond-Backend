eed905d0c2ad831d44ac3cce0ddfd44a4e030958
NguyenDangkh <dangkhoa12305053@gmail.com>
2024-06-08 14:31:51 +0700

    update product

--- DiamondShop/Data/Category.cs
+++ DiamondShop/Data/Category.cs
@@ -10,7 +10,7 @@ namespace DiamondShop.Data
 		public int CategoryId { get; set; }
 		[Required]
 		[StringLength(50)]
-		public string? CategoryName { get; set; }      
+		public string? CategoryName { get; set; } 
         public ICollection<Product> Products { get; } = new List<Product>();
 	}
 }
