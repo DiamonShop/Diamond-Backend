4c6143cdd4adcb24142b0c2b28e640157d45edce
tranducvuong03 <tranducvuong31012003@gmail.com>
2024-06-10 21:46:11 +0700

    fix 3 layer repository

--- DiamondShop/Program.cs
+++ DiamondShop/Program.cs
@@ -24,15 +24,15 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 // Register DbContext
 builder.Services.AddDbContext<DiamondDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Db"));
+	options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
 // Register Identity
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<DiamondDbContext>()
-    .AddSignInManager<SignInManager<IdentityUser>>();
+	.AddEntityFrameworkStores<DiamondDbContext>()
+	.AddSignInManager<SignInManager<IdentityUser>>();
 
 // Register services
 builder.Services.AddScoped<ICartItemRepository, CartItemRepository>();
@@ -47,33 +47,33 @@ builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
 builder.Services.AddCors(options =>
 {
 
-    options.AddPolicy("AllowSpecificOrigin", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
+	options.AddPolicy("AllowSpecificOrigin", policy =>
+	{
+		policy.WithOrigins("http://localhost:3000")
+			  .AllowAnyHeader()
+			  .AllowAnyMethod()
+			  .AllowCredentials();
+	});
 });
 
 // Register JWT Authentication
 var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
 var key = Encoding.ASCII.GetBytes(jwtSettings.SecretKey);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = false;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(key)
-        };
-    });
+	.AddJwtBearer(options =>
+	{
+		options.RequireHttpsMetadata = false;
+		options.SaveToken = true;
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = jwtSettings.Issuer,
+			ValidAudience = jwtSettings.Audience,
+			IssuerSigningKey = new SymmetricSecurityKey(key)
+		};
+	});
 
 // Register Google Authentication
 builder.Services.AddAuthentication(options =>
@@ -84,9 +84,9 @@ builder.Services.AddAuthentication(options =>
 .AddCookie()
 .AddGoogle(options =>
 {
-    options.ClientId = "993387960773-09fmlfavvp7d64eul2122uad7vk70n8f.apps.googleusercontent.com";
-    options.ClientSecret = "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";
-    options.CallbackPath = "/signin-google";
+	options.ClientId = "993387960773-09fmlfavvp7d64eul2122uad7vk70n8f.apps.googleusercontent.com";
+	options.ClientSecret = "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";
+	options.CallbackPath = "/signin-google";
 });
 
 // Register other services and configure the application
@@ -100,31 +100,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Authorization header using the Bearer scheme (\"{token}\")",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT"
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "oauth2"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
+	options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+	{
+		Description = "Standard Authorization header using the Bearer scheme (\"{token}\")",
+		In = ParameterLocation.Header,
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		Scheme = "Bearer",
+		BearerFormat = "JWT"
+	});
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "oauth2"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+	options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+	options.OperationFilter<SecurityRequirementsOperationFilter>();
 });
 
 // Configure and build the application
@@ -132,8 +132,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 // Use CORS to connect front-end
