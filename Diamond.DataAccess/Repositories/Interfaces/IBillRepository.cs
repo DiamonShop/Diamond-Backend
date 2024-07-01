76255560b58ee5dbf4d3569be1003ef7dae7cdd8
khoang <dangkhoa12305053@gmail.com>
2024-07-01 13:22:45 +0700

    hoan thien bill

---
+++ Diamond.DataAccess/Repositories/Interfaces/IBillRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DiamondShop.Data;
+
+namespace DiamondShop.Repositories.Interfaces
+{
+    public interface ICartItemRepository
+    {
+        Task<List<OrderDetail>> GetAll();
+        Task<OrderDetail> GetById(int id);
+        Task<bool> Insert(OrderDetail entity);
+        Task<bool> Update(OrderDetail entity);
+        Task<bool> Delete(int id);
+    }
+}
