06e065d0aa53ecdea2443bbd71641f65c2024470
tranducvuong03 <tranducvuong31012003@gmail.com>
2024-07-29 18:59:21 +0700

    fix error

--- Diamond.DataAccess/Repositories/DiamondPriceRepository .cs
+++ Diamond.DataAccess/Repositories/DiamondPriceRepository .cs
@@ -9,59 +9,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 public class DiamondPriceRepository : IDiamondPriceRepository
+{
+    private readonly DiamondDbContext _context;
+
+    public DiamondPriceRepository(DiamondDbContext context)
     {
-        private readonly DiamondDbContext _context;
-
-        public DiamondPriceRepository(DiamondDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IEnumerable<DiamondPrice>> GetAllDiamondPrices()
-        {
-            return await _context.DiamondPrices.ToListAsync();
-        }
-
-        public async Task<DiamondPrice> GetDiamondPriceById(int id)
-        {
-            return await _context.DiamondPrices.FindAsync(id);
-        }
-
-        public async Task<bool> CreateDiamondPrice(DiamondPrice diamondPrice)
-        {
-            _context.DiamondPrices.Add(diamondPrice);
-            return await _context.SaveChangesAsync() > 0;
-        }
-
-        public async Task<bool> UpdateDiamondPrice(DiamondPrice diamondPrice)
-        {
-            _context.Entry(diamondPrice).State = EntityState.Modified;
-            return await _context.SaveChangesAsync() > 0;
-        }
-
-        public async Task<bool> DeleteDiamondPrice(int id)
-        {
-            var diamondPrice = await _context.DiamondPrices.FindAsync(id);
-            if (diamondPrice == null)
-            {
-                return false;
-            }
-            _context.DiamondPrices.Remove(diamondPrice);
-            return await _context.SaveChangesAsync() > 0;
-        }
-
-        public async Task<IEnumerable<IGrouping<decimal, DiamondPrice>>> GetDiamondsGroupedByDiameter()
-        {
-            return await _context.DiamondPrices
-                                 .GroupBy(dp => dp.DiameterMM)
-                                 .ToListAsync();
-        }
-
-        public async Task<DiamondPrice> GetDiamondPrice(decimal carat, string clarity, string color, string cut)
-        {
-            return await _context.DiamondPrices
-                .FirstOrDefaultAsync(dp => dp.Carat == carat && dp.Clarity == clarity && dp.Color == color && dp.Cut == cut);
-        }
-
+        _context = context;
     }
+
+    public async Task<IEnumerable<DiamondPrice>> GetAllDiamondPrices()
+    {
+        return await _context.DiamondPrices.ToListAsync();
+    }
+
+    public async Task<DiamondPrice> GetDiamondPriceById(int id)
+    {
+        return await _context.DiamondPrices.FindAsync(id);
+    }
+
+    public async Task<bool> CreateDiamondPrice(DiamondPrice diamondPrice)
+    {
+        _context.DiamondPrices.Add(diamondPrice);
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> UpdateDiamondPrice(DiamondPrice diamondPrice)
+    {
+        _context.Entry(diamondPrice).State = EntityState.Modified;
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> DeleteDiamondPrice(int id)
+    {
+        var diamondPrice = await _context.DiamondPrices.FindAsync(id);
+        if (diamondPrice == null)
+        {
+            return false;
+        }
+        _context.DiamondPrices.Remove(diamondPrice);
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<IEnumerable<IGrouping<decimal, DiamondPrice>>> GetDiamondsGroupedByDiameter()
+    {
+        return await _context.DiamondPrices
+                             .GroupBy(dp => dp.DiameterMM)
+                             .ToListAsync();
+    }
+
+    public async Task<DiamondPrice> GetDiamondPrice(decimal carat, string clarity, string color, string cut)
+    {
+        return await _context.DiamondPrices
+            .FirstOrDefaultAsync(dp => dp.Carat == carat && dp.Clarity == clarity && dp.Color == color && dp.Cut == cut);
+    }
+
 }
